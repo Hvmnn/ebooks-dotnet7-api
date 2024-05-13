5c18444798033bdee4d789569b339d3688b1bb96
Your Name <you@example.com>
2024-05-13 14:49:29 -0400

    feat:eBook discount class creation

---
+++ EBookDTO.cs
@@ -0,0 +1,8 @@
+public class EBookDTO
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public string Format { get; set; }
+    public decimal Price { get; set; }
+}
